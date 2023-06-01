196bbbd1114541e22be9d21a8a57b92f8aed9fff
Razvan-Andrei Matisan <57107817+razvanmatisan@users.noreply.github.com>
2023-06-01 23:40:45 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 for seed in 123
 do
-    python run_dt_atari.py --seed $seed --context_length 50 --epochs 10 --model_type 'reward_conditioned' --num_steps 100000 --num_buffers 50 --game 'MontezumaRevenge' --batch_size 64
+    python run_dt_atari.py --seed $seed --context_length 50 --epochs 10 --model_type 'reward_conditioned' --num_steps 1000000 --num_buffers 50 --game 'MontezumaRevenge' --batch_size 64
 done
 
 
